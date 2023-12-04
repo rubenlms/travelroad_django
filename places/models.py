504841450cb6991b50616974b75763d3e7573156
Ruben Lemus <rubenlemushdez@gmail.com>
2023-12-04 16:22:23 +0000

    primer commit

---
+++ places/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from django.db import models
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+    visited = models.BooleanField()
+
+    class Meta:
+        # ↓ necesario porque ya partimos de una tabla creada ↓
+        db_table = "places"
+
+    def __str__(self):
+        return self.name
